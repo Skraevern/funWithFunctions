c6bb7537e60f803c0939519ad4a4e7e46a56bdce
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-04-07 06:21:36 +0200

    Startet on leapYears function

--- script.js
+++ script.js
@@ -181,3 +181,25 @@ let squareSum = function (...theArgs) {
 };
 
 console.log(squareSum(1, 2, 2, 8)); // 73
+
+//
+
+// check if input year is a leap year
+
+const leapYears = function (year) {
+  if (Number.isInteger(year / 4)) {
+    return true;
+  }
+
+  if (Number.isInteger(year / 100) && Number.isInteger(year / 400)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(leapYears(1996));
+
+/*
+Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
+*/
